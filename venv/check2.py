1af5cd8c8c0bdcdf6919f55b990a32cef5683efb
Tonyao1989 <629856469@qq.com>
2020-03-14 21:09:55 +0800

    新增

---
+++ venv/check2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#以下实例为通过用户输入两个数字，并计算两个数字之和：
+# 用户输入数字
+num1=input('请输入第一个数字:')
+num2=input('请输入第二个数字:')
+sum=float(num1)+float(num2)
+print('数字{0}+数字{1}为:{2}'.format(num1,num2,sum))
