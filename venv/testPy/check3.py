c7c9dcf5c45c3861c01f120c8da55300c84d7fcd
Tonyao1989 <629856469@qq.com>
2020-03-14 21:35:33 +0800

    测试平方根脚本

--- venv/testPy/check3.py
+++ venv/testPy/check3.py
@@ -2,4 +2,7 @@
 # -*- coding: UTF-8 -*-
 #平方根，又叫二次方根，表示为〔√￣〕，如：数学语言为：√￣16=4。语言描述为：根号下16=4。
 #以下实例为通过用户输入一个数字，并计算这个数字的平方根：
-num1=input('请输入一个数字：')
+import cmath
+num1=int(input('请输入一个数字：'))
+sqrtqnum=cmath.sqrt(num1)
+print('数字{0}的平方根是{1:0.3f}+{2:0.3f}j'.format(num1,sqrtqnum.real,sqrtqnum.imag))
